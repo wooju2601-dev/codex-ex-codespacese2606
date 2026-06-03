3ee229a0f5e1a643ae8ce5cbf1a4011167562d9d
wooju2601-dev <wooju2601@gmail.com>
2026-06-03 06:35:44 +0000

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  root: 'client',
+  root: path.resolve(__dirname),
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
   server: {
