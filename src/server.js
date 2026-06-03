171fb84f0b2804d7eba4cde4a37e092055e644c4
wooju2601-dev <wooju2601@gmail.com>
2026-06-03 06:27:03 +0000

    fix code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const path = require("path");
+const app = require('./app');
 
-const buildPath = path.join(__dirname, "client", "dist");
+// Render는 PORT 환경 변수를 사용합니다. 로컬에서는 3000번을 사용합니다.
+const port = process.env.PORT || 3000;
 
-app.use(express.static(buildPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
 });
+ㄴ
